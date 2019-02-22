aa1bace7dcb9692d12c1f90c17095be3db5c9d6a
deancomben-okta <deancomben-okta@users.noreply.github.com>
2019-02-22 00:38:39 +0000

    Add files via upload

---
+++ UserMigrationUI/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace UserMigrationUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.start_btn = new System.Windows.Forms.Button();
+            this.delete_btn = new System.Windows.Forms.Button();
+            this.txtBox = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start_btn
+            // 
+            this.start_btn.Location = new System.Drawing.Point(90, 349);
+            this.start_btn.Name = "start_btn";
+            this.start_btn.Size = new System.Drawing.Size(259, 89);
+            this.start_btn.TabIndex = 0;
+            this.start_btn.Text = "Start";
+            this.start_btn.UseVisualStyleBackColor = true;
+            this.start_btn.Click += new System.EventHandler(this.start_btn_Click);
+            // 
+            // delete_btn
+            // 
+            this.delete_btn.Location = new System.Drawing.Point(488, 349);
+            this.delete_btn.Name = "delete_btn";
+            this.delete_btn.Size = new System.Drawing.Size(212, 89);
+            this.delete_btn.TabIndex = 1;
+            this.delete_btn.Text = "Delete";
+            this.delete_btn.UseVisualStyleBackColor = true;
+            this.delete_btn.Click += new System.EventHandler(this.delete_btn_Click);
+            // 
+            // txtBox
+            // 
+            this.txtBox.Location = new System.Drawing.Point(26, 71);
+            this.txtBox.Multiline = true;
+            this.txtBox.Name = "txtBox";
+            this.txtBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtBox.Size = new System.Drawing.Size(750, 248);
+            this.txtBox.TabIndex = 2;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(397, 31);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Number of Users";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.txtBox);
+            this.Controls.Add(this.delete_btn);
+            this.Controls.Add(this.start_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button start_btn;
+        private System.Windows.Forms.Button delete_btn;
+        private System.Windows.Forms.TextBox txtBox;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ UserMigrationUI/Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Okta.Sdk;
+using Okta.Sdk.Configuration;
+using System.Configuration;
+
+namespace UserMigrationUI
+{
+    public partial class Form1 : Form
+    {
+        private List<JObject> userlist;
+        private ILogger logger;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ILoggerFactory loggerFactory = new LoggerFactory()
+                .AddConsole()
+                .AddDebug(LogLevel.Trace);
+            this.logger = loggerFactory.CreateLogger("Logger");
+            this.logger.LogDebug("Starting Logging");
+        }
+
+        private void start_btn_Click(object sender, EventArgs e)
+        {
+
+            //createUsers();
+
+            BlockingCollection<JObject> blockingUserList = BlockingUserList(Convert.ToInt32(numericUpDown1.Value));
+
+            int threads = Convert.ToInt32(ConfigurationManager.AppSettings.Get("Threads"));
+
+            for (int i = 0; i < threads; i++)
+            {
+                Task createUserTask = Task.Run(() => NonBlockingCreateUser(blockingUserList));
+            }
+
+        }
+
+        private async void NonBlockingCreateUser(BlockingCollection<JObject> bc)
+        {
+            var threadID = Thread.CurrentThread.ManagedThreadId;
+            while (!bc.IsCompleted)
+            {
+                var configuration = new Okta.Sdk.Configuration.OktaClientConfiguration
+                {
+                    OktaDomain = ConfigurationManager.AppSettings.Get("OktaOrgURL"),
+                    Token = ConfigurationManager.AppSettings.Get("OktaAPIKey"),
+                    MaxRetries = 10,
+                    ConnectionTimeout = 5,
+                    RequestTimeout = 120
+                };
+                //var client = new OktaClient(config);
+
+                var httpClient = new System.Net.Http.HttpClient();
+                var maxRetries = configuration.MaxRetries ?? OktaClientConfiguration.DefaultMaxRetries;
+                var requestTimeout = configuration.RequestTimeout ?? OktaClientConfiguration.DefaultRequestTimeout;
+
+                var client = new OktaClient(apiClientConfiguration: configuration, httpClient: httpClient, logger: this.logger, retryStrategy: new CustomRetryStrategy(maxRetries, requestTimeout, 1, logger));
+
+                JObject user = null;
+                try
+                {
+                    user = bc.Take();
+                }
+                catch (InvalidOperationException)
+                {
+
+                }
+                if (user != null)
+                {
+                    //process
+                    var uid = user.Value<JToken>("profile").Value<string>("login");
+                    threadID = Thread.CurrentThread.ManagedThreadId;
+                    //Console.WriteLine($"ThreadID: {threadID}, User: {uid}");
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        txtBox.AppendText($"ThreadID: {threadID}, Request: CreateUser, User: {uid}{Environment.NewLine}");
+                    });
+
+                    try
+                    {
+                        var result = await client.PostAsync<User>(new HttpRequest
+                        {
+                            Uri = $"/api/v1/users",
+                            QueryParameters = new Dictionary<string, object>()
+                            {
+                                ["activate"] = false,
+                            },
+                            Payload = user
+                        });
+
+                        threadID = Thread.CurrentThread.ManagedThreadId;
+                        //Console.WriteLine($"ThreadID: {threadID}, User: {uid}, Status: {result.Status}");
+                        this.Invoke((MethodInvoker)delegate ()
+                        {
+                            txtBox.AppendText($"ThreadID: {threadID}, Response: CreateUser, User: {uid}, Status: {result.Status}{Environment.NewLine}");
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        this.Invoke((MethodInvoker)delegate ()
+                        {
+                            txtBox.AppendText(ex.Message + Environment.NewLine);
+                        });
+                    }
+                }
+                //Thread.SpinWait(5000000);
+            }
+            threadID = Thread.CurrentThread.ManagedThreadId;
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                txtBox.AppendText($"ThreadID: {threadID}, Processing Done!");
+            });
+        }
+
+        private async void NonBlockingDeleteUser(BlockingCollection<JObject> bc)
+        {
+            var threadID = Thread.CurrentThread.ManagedThreadId;
+            while (!bc.IsCompleted)
+            {
+                var configuration = new Okta.Sdk.Configuration.OktaClientConfiguration
+                {
+                    OktaDomain = ConfigurationManager.AppSettings.Get("OktaOrgURL"),
+                    Token = ConfigurationManager.AppSettings.Get("OktaAPIKey"),
+                    MaxRetries = 10,
+                    ConnectionTimeout = 5,
+                    RequestTimeout = 120
+                };
+                //var client = new OktaClient(config);
+
+                var httpClient = new System.Net.Http.HttpClient();
+                var maxRetries = configuration.MaxRetries ?? OktaClientConfiguration.DefaultMaxRetries;
+                var requestTimeout = configuration.RequestTimeout ?? OktaClientConfiguration.DefaultRequestTimeout;
+
+                var client = new OktaClient(apiClientConfiguration: configuration, httpClient: httpClient, logger: this.logger, retryStrategy: new CustomRetryStrategy(maxRetries, requestTimeout, 1, logger));
+
+                JObject user = null;
+                try
+                {
+                    user = bc.Take();
+                }
+                catch (InvalidOperationException)
+                {
+
+                }
+                if (user != null)
+                {
+                    //process
+                    var uid = user.Value<JToken>("profile").Value<string>("login");
+                    threadID = Thread.CurrentThread.ManagedThreadId;
+                    //Console.WriteLine($"ThreadID: {threadID}, User: {uid}");
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        txtBox.AppendText($"ThreadID: {threadID}, Request: DeleteUser, User: {uid}{Environment.NewLine}");
+                    });
+
+                    try
+                    {
+                        // Get User
+                        var u = await client.Users.GetUserAsync(uid);
+
+                        // First, deactivate the user
+                        await u.DeactivateAsync();
+
+                        // Then delete the user
+                        await u.DeactivateOrDeleteAsync();
+
+
+
+                        threadID = Thread.CurrentThread.ManagedThreadId;
+                        this.Invoke((MethodInvoker)delegate ()
+                        {
+                            txtBox.AppendText($"ThreadID: {threadID}, Response: DeleteUser, User: {uid}, Status: DELETED{Environment.NewLine}");
+                        });
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        this.Invoke((MethodInvoker)delegate ()
+                        {
+                            txtBox.AppendText(ex.Message + Environment.NewLine);
+                        });
+                    }
+                }
+                //Thread.SpinWait(5000000);
+            }
+            threadID = Thread.CurrentThread.ManagedThreadId;
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                txtBox.AppendText($"ThreadID: {threadID}, Processing Done!");
+            });
+        }
+
+        private async void createUsers()
+        {
+            // Get lots of users
+            userlist = UserList(Convert.ToInt32(numericUpDown1.Value));
+
+            var configuration = new Okta.Sdk.Configuration.OktaClientConfiguration
+            {
+                OktaDomain = ConfigurationManager.AppSettings.Get("OktaOrgURL"),
+                Token = ConfigurationManager.AppSettings.Get("OktaAPIKey"),
+                MaxRetries = 10,
+                ConnectionTimeout = 5,
+                RequestTimeout = 120
+            };
+            //var client = new OktaClient(config);
+
+            var httpClient = new System.Net.Http.HttpClient();
+            var maxRetries = configuration.MaxRetries ?? OktaClientConfiguration.DefaultMaxRetries;
+            var requestTimeout = configuration.RequestTimeout ?? OktaClientConfiguration.DefaultRequestTimeout;
+
+            var client = new OktaClient(apiClientConfiguration: configuration, httpClient: httpClient, logger: this.logger, retryStrategy: new DefaultRetryStrategy(maxRetries, requestTimeout));
+
+            foreach (var user in userlist)
+            {
+
+                try
+                {
+                    var result = await client.PostAsync<User>(new HttpRequest
+                    {
+                        Uri = $"/api/v1/users",
+                        QueryParameters = new Dictionary<string, object>()
+                        {
+                            ["activate"] = false,
+                        },
+                        Payload = user
+                    });
+
+                    Console.WriteLine($"Login: {result.Profile.Login}, Status: {result.Status}");
+                    txtBox.AppendText($"Login: {result.Profile.Login}, Status: {result.Status}{Environment.NewLine}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    txtBox.AppendText(ex.Message + Environment.NewLine);
+                }
+            } //end foreach
+        }
+
+        static List<JObject> UserList(int count)
+        {
+            List<JObject> users = new List<JObject>();
+            for (int i = 0; i < count; i++)
+            {
+                var jsonObject = new JObject();
+                dynamic user = jsonObject;
+                user.profile = new JObject() as dynamic;
+                user.profile.firstName = $"Anakin{i}";
+                user.profile.lastName = $"Skywalker{i}";
+                user.profile.email = $"darth.vader{i}@imperial-senate.gov";
+                user.profile.login = $"darth.vader{i}@imperial-senate.gov";
+                user.profile.memberNumber = "474839294";
+                user.credentials = new JObject() as dynamic;
+                user.credentials.password = new JObject() as dynamic;
+                //user.credentials.password.value = "D1sturB1ng!";
+                user.credentials.password.hash = new JObject() as dynamic;
+                user.credentials.password.hash.algorithm = "BCRYPT";
+                user.credentials.password.hash.workFactor = 10;
+                user.credentials.password.hash.salt = "rwh3vH166HCH/NT9XV5FYu";
+                user.credentials.password.hash.value = "qaMqvAPULkbiQzkTCWo5XDcvzpk8Tna";
+                user.groupIds = new JArray();
+                user.groupIds.Add("00gjcgmj8gGinzhpW0h7"); //Replace with Okta Group ID
+
+                users.Add(user);
+            }
+
+            return users;
+        }
+
+        static BlockingCollection<JObject> BlockingUserList(int count)
+        {
+            BlockingCollection<JObject> users = new BlockingCollection<JObject>();
+            for (int i = 0; i < count; i++)
+            {
+                var jsonObject = new JObject();
+                dynamic user = jsonObject;
+                user.profile = new JObject() as dynamic;
+                user.profile.firstName = $"Anakin{i}";
+                user.profile.lastName = $"Skywalker{i}";
+                user.profile.email = $"darth.vader{i}@imperial-senate.gov";
+                user.profile.login = $"darth.vader{i}@imperial-senate.gov";
+                user.profile.memberNumber = "474839294";
+                user.credentials = new JObject() as dynamic;
+                user.credentials.password = new JObject() as dynamic;
+                //user.credentials.password.value = "D1sturB1ng!";
+                user.credentials.password.hash = new JObject() as dynamic;
+                user.credentials.password.hash.algorithm = "BCRYPT";
+                user.credentials.password.hash.workFactor = 10;
+                user.credentials.password.hash.salt = "rwh3vH166HCH/NT9XV5FYu";
+                user.credentials.password.hash.value = "qaMqvAPULkbiQzkTCWo5XDcvzpk8Tna";
+                user.groupIds = new JArray();
+                user.groupIds.Add("00gjcgmj8gGinzhpW0h7"); //Replace with Okta Group ID
+
+                users.Add(user);
+            }
+
+            return users;
+        }
+
+        private async void delete_btn_Click(object sender, EventArgs e)
+        {
+            //DeleteUsers();
+
+            BlockingCollection<JObject> blockingUserList = BlockingUserList(Convert.ToInt32(numericUpDown1.Value));
+
+            int threads = Convert.ToInt32(ConfigurationManager.AppSettings.Get("Threads"));
+
+            for (int i = 0; i < threads; i++)
+            {
+                Task deleteUsersTask = Task.Run(() => NonBlockingDeleteUser(blockingUserList));
+            }
+
+        }
+
+        private async void DeleteUsers()
+        {
+            // Get lots of users
+            userlist = UserList(Convert.ToInt32(numericUpDown1.Value));
+
+            var configuration = new Okta.Sdk.Configuration.OktaClientConfiguration
+            {
+                OktaDomain = ConfigurationManager.AppSettings.Get("OktaOrgURL"),
+                Token = ConfigurationManager.AppSettings.Get("OktaAPIKey"),
+                MaxRetries = 10,
+                ConnectionTimeout = 5,
+                RequestTimeout = 120
+            };
+            //var client = new OktaClient(config);
+
+            var httpClient = new System.Net.Http.HttpClient();
+            var maxRetries = configuration.MaxRetries ?? OktaClientConfiguration.DefaultMaxRetries;
+            var requestTimeout = configuration.RequestTimeout ?? OktaClientConfiguration.DefaultRequestTimeout;
+
+            var client = new OktaClient(apiClientConfiguration: configuration, httpClient: httpClient, logger: this.logger, retryStrategy: new DefaultRetryStrategy(maxRetries, requestTimeout));
+
+
+            foreach (var user in userlist)
+            {
+                try
+                {
+                    // Get User
+                    var uid = user.Value<JToken>("profile").Value<string>("login");
+                    var u = await client.Users.GetUserAsync(uid);
+
+                    // First, deactivate the user
+                    await u.DeactivateAsync();
+
+                    // Then delete the user
+                    await u.DeactivateOrDeleteAsync();
+
+                    Console.WriteLine($"Login: {u.Profile.Login}, Status: DELETED");
+                    txtBox.AppendText($"Login: {u.Profile.Login}, Status: DELETED {Environment.NewLine}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    txtBox.AppendText(ex.Message + Environment.NewLine);
+                }
+            }
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
